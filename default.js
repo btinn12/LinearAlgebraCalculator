54c9c3cca86b95c6193f701c1b8218fb994a57de
Blake Tinney <blake.tinney@yahoo.com>
2016-09-28 20:10:45 -0700

    Refactor default.js

--- default.js
+++ default.js
@@ -4,17 +4,20 @@ $(document).ready(function() {
     this.rows = rows;
     this.cols = cols;
     this.val = [];
-    this.initialize = function() {
-      for (var i = 0; i < rows; i++) {
+    this.initialize();
+
+  }
+  
+  Matrix.prototype.get = function(r, c) {
+    return this.val[r][c];
+  }
+  
+  Matrix.prototype.initialize = function() {
+      for (var i = 0; i < this.rows; i++) {
           this.val[i] = new Array();
-          for (var j = 0; j < cols; j++) {
+          for (var j = 0; j < this.cols; j++) {
               this.val[i][j] = 0;
           }
       }
     };
-    this.get = function(r, c) {
-        return this.val[r][c];
-    };
-
-  }
 });
