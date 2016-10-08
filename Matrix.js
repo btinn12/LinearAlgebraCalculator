77bd82235b996761d63babac8621b923f9d723a6
Blake Tinney <blake.tinney@yahoo.com>
2016-10-08 16:00:58 -0700

    Branch determinant and add to Matrix.js

--- Matrix.js
+++ Matrix.js
@@ -36,3 +36,53 @@ function matrixMult(A, B) {
 	}
 	return C;
 }
+
+function add(m1, m2)
+{
+	var sum = new Matrix(m1.rows, m1.cols)
+	for(var i = 0;i<m1.rows;i++)
+	{
+		for(var j = 0;j<m1.cols;j++)
+		{
+			var plus = parseInt(m1.get(i,j)) + parseInt(m2.get(i,j));
+			sum.set(plus, i, j);
+		}
+	}
+	return sum;
+}
+
+function determinant(matrix, n)
+{
+	if(n == 2)
+	{
+		return matrix.get(0,0)*matrix.get(1,1) - matrix.get(0,1)*matrix.get(1,0);
+	}
+	else 
+	{
+		var sum = 0;
+		for(var i = 0;i<n;i++)
+		{
+			sum += Math.pow(-1,i)*matrix.get(0,i)*determinant(createCofactor(matrix, i), n-1);
+		}
+		return sum;
+	}
+}
+
+function createCofactor(matrix, col)
+{
+	var cofactor = new Matrix(matrix.rows - 1,matrix.cols - 1);
+	var coIndex = 0;
+	for(var i = 0;i<matrix.cols;i++)
+	{
+		if(i == col){}
+		else
+		{
+			for(var j = 1;j<matrix.rows;j++)
+			{
+				cofactor.set(matrix.get(j,i), j-1, coIndex);
+			}
+			coIndex++;
+		}
+	}
+	return cofactor;
+}
--- default.js
+++ default.js
@@ -44,20 +44,6 @@ $(document).ready(function () {
 		}	
 	}
 	
-	function add(m1, m2)
-	{
-		var sum = new Matrix(m1.rows, m1.cols)
-		for(var i = 0;i<m1.rows;i++)
-		{
-			for(var j = 0;j<m1.cols;j++)
-			{
-				var plus = parseInt(m1.get(i,j)) + parseInt(m2.get(i,j));
-				sum.set(plus, i, j);
-			}
-		}
-		return sum;
-	}
-	
 	function handleDeterminant() {
 		var matrix = retrieveMatrix("m1");
 		var output = determinant(matrix, matrix.rows);
@@ -65,42 +51,6 @@ $(document).ready(function () {
 		outputMatrix.set(output, 0, 0);
 		addOutputMatrix(outputMatrix);
 		
-	}
-				
-	function determinant(matrix, n)
-	{
-		if(n == 2)
-		{
-			return matrix.get(0,0)*matrix.get(1,1) - matrix.get(0,1)*matrix.get(1,0);
-		}
-		else 
-		{
-			var sum = 0;
-			for(var i = 0;i<n;i++)
-			{
-				sum += Math.pow(-1,i)*matrix.get(0,i)*determinant(createCofactor(matrix, i), n-1);
-			}
-			return sum;
-		}
-	}
-	
-	function createCofactor(matrix, col)
-	{
-		var cofactor = new Matrix(matrix.rows - 1,matrix.cols - 1);
-		var coIndex = 0;
-		for(var i = 0;i<matrix.cols;i++)
-		{
-			if(i == col){}
-			else
-			{
-				for(var j = 1;j<matrix.rows;j++)
-				{
-					cofactor.set(matrix.get(j,i), j-1, coIndex);
-				}
-				coIndex++;
-			}
-		}
-		return cofactor;
 	}
 	
 	function handleAddMatrix() {
